c9d772a3b42997e8866f32a2008a152cdcdab3e5
Emil Folino <emilfolino@gmail.com>
2026-03-06 13:57:45 +0100

    Initial commit, up and running

---
+++ app.mjs
@@ -0,0 +1,23 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+
+import database from './db/database.mjs'
+
+const app = express()
+
+app.disable('x-powered-by')
+
+app.set("view engine", "ejs")
+
+const port = process.env.PORT || 8666
+
+
+app.use(bodyParser.json({limit: '50mb'})) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static(path.join(process.cwd(), "public")))
+
+app.get('/', (req, res) => res.redirect('/documentation.html'))
+
+app.listen(port, () => console.log('Order api listening on port ' + port))
---
+++ db/database.mjs
@@ -0,0 +1,19 @@
+import sqlite3 from "sqlite3"
+import { open } from "sqlite"
+
+const database = {
+  openDb: async function openDb(filename = "docs.sqlite") {
+    let dbFilename = `./db/${filename}`
+
+    if (process.env.NODE_ENV === "test") {
+      dbFilename = "./db/test.sqlite"
+    }
+
+    return await open({
+      filename: dbFilename,
+      driver: sqlite3.Database,
+    })
+  },
+}
+
+export default database
