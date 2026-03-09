3c526d949fdac72e5483adc20c5a3cb4adab4a02
Emil Folino <emilfolino@gmail.com>
2026-03-09 08:06:21 +0100

    Fixed errorhandling and adding of projects

--- models/projects.mjs
+++ models/projects.mjs
@@ -1,4 +1,5 @@
 import database from "../db/database.mjs"
+import hat from "hat"
 
 export default {
   getProjects: async function getProjects(email, apiKey) {
@@ -18,10 +19,35 @@ export default {
         message: error.message,
       }}
     }
-
   },
 
   createProject: async function createProject(name, email, apiKey) {
+    const db = await database.openDb()
 
+    try {
+      const uid = hat()
+      await db.run(
+        `INSERT INTO projects (uid, name) VALUES (?, ?)`,
+        uid,
+        name,
+      )
+
+      await db.run(
+        `INSERT INTO user_projects (uid, email, api_key) VALUES (?, ?, ?)`,
+        uid,
+        email,
+        apiKey,
+      )
+      
+      return {
+        uid: uid,
+        name: name,
+      }
+    } catch (error) {
+      return { errors: {
+        title: "Database error",
+        message: error.message,
+      }}
+    }
   }
 }
--- routes/projects.mjs
+++ routes/projects.mjs
@@ -8,18 +8,26 @@ import projectsModel from "../models/projects.mjs"
 router.get("/", 
   (req, res, next) => auth.checkToken(req, res, next),
   async (req, res) => {
-    const projects = await projectsModel.getProjects(req.user.email, req.user.api_key)
+    const results = await projectsModel.getProjects(req.user.email, req.user.api_key)
 
-    return res.json({ data: projects })
+    if ("errors" in results) {
+      return res.status(500).json({ data: results })
+    }
+
+    return res.json({ data: results })
   }
 )
 
 router.post("/", 
   (req, res, next) => auth.checkToken(req, res, next),
   async (req, res) => {
-    const project = await projectsModel.createProject(req.body.name, req.user.email, req.user.api_key)
+    const results = await projectsModel.createProject(req.body.name, req.user.email, req.user.api_key)
 
-    return res.json({ data: project })
+    if ("errors" in results) {
+      return res.status(500).json({ data: results })
+    }
+
+    return res.json({ data: results })
   }
 )
 
